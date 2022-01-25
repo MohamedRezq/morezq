62794f47cbb613993bb054e9fbd27315fbee429f
MohamedRezq <mohamed.rizq99@gmail.com>
2022-01-25 23:22:44 +0200

    fixed tailwind styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './sections/**/*.{js,ts,jsx,tsx}'],
+  darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
